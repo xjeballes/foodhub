d1f424443829469eb4d03fb8e8b66a9975cec9b6
stevelclar <steveel.clar@g.msuiit.edu.ph>
2018-11-29 13:47:25 +0800

    updated

--- blueprints/RegisterBlueprint.py
+++ blueprints/RegisterBlueprint.py
@@ -4,13 +4,16 @@ import mysql.connector
 from forms.RegistrationForm import RegistrationForm
 from classes.User import User
 import mysql.connector
+from flask_bcrypt import Bcrypt
 
+bcrypt = Bcrypt()
 
 register_blueprint = Blueprint('register_blueprint', __name__)
 mydb = DB().conn()
-mycursor = mydb.cursor()
-@register_blueprint.route("/registration", methods=['GET', 'POST'])
-def registration():
+mycursor = mydb.cursor(dictionary=True)
+
+@register_blueprint.route("/registration/customer", methods=['GET', 'POST'])
+def registration_customer():
     if('user' in session):
         print 'Found user in session: ', session['user']
         return redirect(url_for('login_blueprint.login'))
@@ -22,42 +25,123 @@ def registration():
         contact_number = request.form["contact_number"]
         username= request.form["username"]
         email = request.form["email"]
-        password = request.form["password"]
+        password = bcrypt.generate_password_hash(request.form["password"]) 
+
+        sql = "INSERT INTO customer ( firstname,lastname,gender,contact_number,username, email, password) VALUES (%s, %s, %s, %s, %s, %s,%s)"
+        val = (firstname,lastname,gender,contact_number,username,email,password)
+        mycursor.execute(sql, val)
+        mydb.commit()  
+        sql1 = "SELECT user_id as id,firstname,lastname,gender,contact_number,username,email from customer where username=%s"
+        
+        mycursor.execute(sql1, [username])
+        user = mycursor.fetchone()
+        mydb.commit()
+        session['user'] = user
+        return redirect(url_for('home_blueprint.home'))
+
+    elif request.method == 'POST':
+        return render_template('register_customer.html', form=form,error=True)
+
+    return render_template('register_customer.html', form=form,)
+
+
+@register_blueprint.route("/registration/owner", methods=['GET', 'POST'])
+
+def registration_owner():
+    if('user' in session):
+        print 'Found user in session: ', session['user']
+        return redirect(url_for('login_blueprint.login'))
+
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+        gender = request.form["gender"]
+        contact_number = request.form["contact_number"]
+        username= request.form["username"]
+        email = request.form["email"]
+        password = bcrypt.generate_password_hash(request.form["password"])
+        restaurant_name = request.form["restaurant_name"]
+        restaurant_type = request.form["restaurant_type"]
+        bio = request.form["bio"]
+        locations = request.form["locations"]
+
+
+        sql1 = "INSERT INTO owner ( firstname,lastname,gender,contact_number,username, email, password) VALUES (%s, %s, %s, %s, %s, %s,%s)"
+        val1 = (firstname,lastname,gender,contact_number,username,email,password)
+        mycursor.execute(sql1, val1)
+        mydb.commit()  
+
+        sql2 = "INSERT INTO restaurant ( restaurant_name,restaurant_type,bio,locations) VALUES (%s, %s, %s, %s)"
+        val2 = (restaurant_name,restaurant_type,bio,locations)
+        mycursor.execute(sql2, val2)
+        mydb.commit()  
+
+        sql3 = "SELECT owner_id as id,firstname,lastname,gender,contact_number,username,email from owner where username=%s"
+        mycursor.execute(sql3, [username])
+        user = mycursor.fetchone()
+
+        mydb.commit()
+        session['user'] = user
+        return redirect(url_for('home_blueprint.home'))
+
+    elif request.method == 'POST':
+        return render_template('register_owner.html', form=form,error=True)
+
+    return render_template('register_owner.html', form=form,)
+
+
+@register_blueprint.route("/registration", methods=['GET', 'POST'])
+def registration():
+    error = False
+    if('user' in session):
+        print 'Found user in session: ', session['user']
+        return redirect(url_for('login_blueprint.login'))
+    form = RegistrationForm(request.form)
+    if request.method == 'POST' and form.validate():
+        firstname = request.form["firstname"]
+        lastname = request.form["lastname"]
+        gender = request.form["gender"]
+        contact_number = request.form["contact_number"]
+        username= request.form["username"]
+        email = request.form["email"]
+        password = bcrypt.generate_password_hash(request.form["password"]) 
         type=request.form['type']
         try:
 
             if(type == "owner"):
-                sql = "INSERT INTO owner ( firstname,lastname,gender,contact_number,username, email, password) VALUES (%s, %s, %s, %s, %s, %s,%s)"
+                sql1 = "INSERT INTO owner ( firstname,lastname,gender,contact_number,username, email, password) VALUES (%s, %s, %s, %s, %s, %s,%s)"
+                sql2 =  "INSERT INTO restaurant ( restaurant_name,restaurant_type,bio,locations) VALUES (%s, %s, %s, %s, %s)"
             else:
                 sql = "INSERT INTO customer ( firstname,lastname,gender,contact_number,username, email, password) VALUES (%s, %s, %s, %s, %s, %s,%s)"
             
-            val = (firstname,lastname,gender,contact_number,username,email,password)
+            val1 = (firstname,lastname,gender,contact_number,username,email,password)
+            val2 = (restaurant_name, restaurant_type, bio, locations)
             
             
-            mycursor.execute(sql, val)
+            mycursor.execute(sql1, val1)
+            mycursor.execute(sql2, val2)
             mydb.commit()
 
 
             if(type == "owner"):
-                sql = "SELECT owner_id as id,firstname,lastname,gender,contact_number,username,email from owner where username=%s"
+                sql1 = "SELECT owner_id as id,firstname,lastname,gender,contact_number,username,email from owner where username=%s"
+                sql2 = "SELECT restaurant_id as id,restaurant_name,restaurant_type,bio,locations from restaurant where username=%s"
             else:
-                sql = "SELECT user_id as id,firstname,lastname,gender,contact_number,username,email from customer where username=%s"
+                sql1 = "SELECT user_id as id,firstname,lastname,gender,contact_number,username,email from customer where username=%s"
         
 
             
-            mycursor.execute(sql, [username])
-            myresult = mycursor.fetchall()
+            mycursor.execute(sql1, [username])
+            user = mycursor.fetchone()
             
             mydb.commit()
-            print(myresult)
-            for id,firstname,lastname,gender,contact_number,username,email in myresult:
-                user = User(id,firstname,lastname,gender,contact_number,username,email,type)
-            print user.toJSON()
-            session['user'] = vars(user)
+            session['user'] = user
             return redirect(url_for('home_blueprint.home'))
         except mysql.connector.Error as err:
+            error = True
             print(err)
     elif request.method == 'POST':
         return render_template('registration.html', form=form,error=True)
 
-    return render_template('registration.html', form=form,)
+    return render_template('registration.html', form=form,error=error)
--- forms/RegistrationForm.py
+++ forms/RegistrationForm.py
@@ -37,16 +37,20 @@ def unique_email(form, field):
     return True
     
 class RegistrationForm(Form):
-    firstname = StringField('Firstname', [validators.DataRequired(),validators.Length(min=1, max=25,message="First Name must be between 1 and 25 characters long.")])
-    lastname = StringField('Lastname', [validators.DataRequired(),validators.Length(min=1, max=25,message="Last Name must be between 1 and 25 characters long.")])
-    gender = SelectField('Gender', choices=[('Male','Male'),('Female','Female')])
-    contact_number =StringField('Contact Number', [validators.DataRequired(),validators.Length(min=11, max=11,message="Contact must be 11 characters long.")])
-    username = StringField('Username', [validators.DataRequired(),validators.Length(min=1, max=25,message="Username must be between 1 and 25 characters long."),unique_username])
-    email = StringField('Email Address', [validators.DataRequired(),validators.Length(min=6, max=35,message="Email must be between 6 and 35 characters long."),unique_email])
-    password = PasswordField('New Password', [
+    firstname = StringField('First Name:', [validators.DataRequired(),validators.Length(min=1, max=25,message="First Name must be between 1 and 25 characters long.")])
+    lastname = StringField('Last Name:', [validators.DataRequired(),validators.Length(min=1, max=25,message="Last Name must be between 1 and 25 characters long.")])
+    gender = RadioField('Gender:',choices=[('Male','Male'),('Female','Female')])
+    contact_number =StringField('Contact Number:', [validators.DataRequired(),validators.Length(min=11, max=11,message="Contact must be 11 characters long.")])
+    username = StringField('Username:', [validators.DataRequired(),validators.Length(min=5, max=25,message="Username must be between 5 and 25 characters long."),unique_username])
+    email = StringField('Email Address:', [validators.DataRequired(),validators.Length(min=6, max=35,message="Email must be between 6 and 35 characters long."),unique_email])
+    password = PasswordField('Password:', [
         validators.DataRequired(),
         validators.EqualTo('cpassword', message='Passwords must match')
     ])
-    cpassword = PasswordField('Repeat Password',[validators.DataRequired()])
+    cpassword = PasswordField('Repeat Password:',[validators.DataRequired()])
     type = RadioField('',[validators.DataRequired(message="Please select account type.")],choices=[('customer','customer'),('owner','owner')])
+    restaurant_name = StringField('Restaurant Name:', [validators.Length(min=0, max=25,message="Restaurant Name must be between 1 and 25 characters long.")])
+    restaurant_type = StringField('Restaurant Type:', [validators.Length(min=0, max=25,message="Restaurant Type must be between 1 and 25 characters long.")])
+    bio = StringField('Bio:', [validators.Length(min=0, max=150,message="Bio must be between 1 and 150 characters long.")])
+    locations = StringField('Locations:', [validators.Length(min=0, max=25,message="Locations must be between 1 and 25 characters long.")])
 
