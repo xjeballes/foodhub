17445992d2a66792b4bd3e5590bc94ab21af2758
John Benedict Solomon <johnbenedict.solomon@g.msuiit.edu.ph>
2019-01-08 13:44:53 +0800

    add search feature

--- forms/ReviewForm.py
+++
@@ -1,9 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField, TextAreaField
-from wtforms.validators import DataRequired
-
-
-class ReviewForm(FlaskForm):
-	title = StringField('Title', validators=[DataRequired()])
-	content = TextAreaField('Review', validators=[DataRequired()])
-	submit = SubmitField('Post')
